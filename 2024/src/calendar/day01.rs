a33683ba6e580fccb9ee49fe59f1eb9c751b5d22
Rithy Muth <rithy@macro.com>
2024-12-02 00:11:20 -0500

    Add Day 1 Part 2

--- 2024/src/calendar/day01.rs
+++ 2024/src/calendar/day01.rs
@@ -20,6 +20,28 @@ pub fn day01() {
     for i in 0..count {
         total_distance += (left[i] - right[i]).abs();
     }
-
     println!("Total Distance: {}", total_distance);
+
+    let mut similarity_score = 0;
+    let mut right_it = right.iter().peekable();
+    for left_num in left {
+        let mut count = 0;
+
+        while let Some(&right_num) = right_it.peek() {
+            if *right_num > left_num {
+                break;
+            }
+
+            if *right_num < left_num {
+                right_it.next();
+                continue;
+            }
+
+            count += 1;
+            right_it.next();
+        }
+
+        similarity_score += left_num * count;
+    }
+    println!("Similarity Score: {}", similarity_score);
 }
